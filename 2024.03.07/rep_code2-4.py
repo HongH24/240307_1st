70516ce9a24a381e8149180d88291adfe24682fd
HongH24 <kim.honghee93@gmail.com>
2024-03-08 18:10:37 +0900

    first commit

---
+++ 2024.03.07/rep_code2-4.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  8 17:53:03 2024
+
+@author: honghee
+"""
+
+a = int(input("첫 번째 숫자를 입력하세요 :"))
+b = int(input("두 번째 숫자를 입력하세요 :"))
+
+result = a + b
+print(a, "+", b, "=", result)
+
+result = a - b
+print(a, "-", b, "=", result)
+
+result = a * b
+print(a, "*", b, "=", result)
+
+result = a / b
+print(a, "/", b, "=", result)
