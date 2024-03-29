8ce4b659c8660b76b3d5725469de91ce4b0a8b5d
HongH24 <162521050+HongH24@users.noreply.github.com>
2024-03-29 15:27:32 +0900

    Add files via upload

---
+++ 4th/assignment1.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 10:01:08 2024
+
+@author: honghee
+"""
+
+#%% Q5
+print('[ assignment1-Q5 ]')
+ss = "Python"
+for i in range(0,len(ss)):
+    print(ss[i]+'$', end='')
+    
+print('\n')
+    
+# A) (1) for 
+#    (2) len(ss) 
+#    (3) ss[i]
+
+
+#%% Q11
+
+
+print('[ assignment1-Q11 ]')
+inStr  = "파이썬 ### CookBook $$$ @@@ 열공중 123"
+outStr = ""
+
+for i in range(len(inStr)):
+                
+    if not inStr[i].isspace() \
+        and not inStr[i].isdigit() \
+        and inStr[i] not in "#$@":
+        outStr += inStr[i]
+        
+print("기존 문자열 --> %s"%inStr)
+print("한글, 영문자만 남김 --> %s"%outStr)
+print('\n')
+
+#%% Q9
+print('[ assignment1-Q9 ]')
+inStr, outStr = "Python", ""
+strLen = len(inStr)
+
+for i in range(0, strLen):
+    outStr += inStr[len(inStr) - (i+1)]
+    
+print("내용을 거꾸로 출력 --> %s"%outStr)
+print('\n')
+
+
+#%% Q13
+print('[ assignment1-Q13 ]')
+
+
+## exercise program2 - Code08-07.py
+import turtle
+import random
+from tkinter.simpledialog import *
+import math
+
+inStr = ""
+swidth, sheight = 500, 500
+tx, ty, txtsize = 0, 0, 20
+
+inStr = askstring('문자열입력','거북이 쓸 문자열 입력')
+# 나 보기가 역겨워 가실 때에는 말없이 고이 보내 드리오리다 영변에 약산 진달래꽃 아름 따다 가실 길에 뿌리오리다
+
+radius = 200
+rotations = 2 ## 회전수
+angle = 360 * rotations / len(inStr)
+radian = 3.14 * angle / 180
+
+turtle.title('거북이가 나선모양으로 글자쓰기')
+turtle.shape('turtle')
+turtle.setup(width = swidth+50, height = sheight+50)
+turtle.screensize(swidth, sheight)
+turtle.penup()
+
+
+for ch in inStr:
+    tx = radius * math.cos(radian)
+    ty = radius * math.sin(radian)
+    r  = random.random()
+    g  = random.random()
+    b  = random.random()
+
+    turtle.goto(tx, ty)
+    
+    turtle.pencolor( (r,g,b) )
+    turtle.write(ch, font=('맑은고딕', txtsize, 'bold'))
+    
+    radius -= 3  # Increase (+)  // Decrease (-)
+    radian += 3.14 * angle / 180
+    
+turtle.done()
+
+
---
+++ 4th/assignment2.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 30 10:11:42 2024
+
+@author: honghee
+"""
+
+#%% Q3
+print('[ assignment2-Q3 ]')
+def plus(v1,v2,v3):
+    result = 0
+    result = v1+v2+v3
+    return result
+    
+hap = plus(100,200,300)
+print(hap)
+print('\n')
+
+## (1) v1,v2,v3  (2) return result
+
+#%% Q4
+print('[ assignment2-Q4 ]')
+def f1():
+    print(var)
+def f2():
+    var=10
+    print(var)
+var=100
+f1()
+f2()
+
+## guess : 100, 10
+## answer : 100, 10
+print('\n')
+
+#%% Q11
+print('[ assignment2-Q11 ]')
+def addNumber(num):
+    snum=0
+    for i in range(1,num+1):
+        snum += i
+    print(snum)
+print(addNumber(100))
+print('\n')
+
+## answer >> def complete the blank
+
+
+#%% Q8
+print('[ assignment1-Q8 ]')
+def myFunc(p1=1,p2=2,p3=3):
+    ret = p1 + p2 + p3
+    return ret
+
+print("매개변수 없이 호출 ==> ", myFunc())
+print("매개변수 1개로 호출 ==> ", myFunc(1))
+print("매개변수 2개로 호출 ==> ", myFunc(1,2))
+print("매개변수 3개로 호출 ==> ", myFunc(1,2,3))
+
+## answer >> p1=1,p2=2,p3=3
